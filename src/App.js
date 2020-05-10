a34d1bb372cb9d2c02286ba2f367f185c64b6337
jasonbringle <jasonbringle@gmail.com>
2020-05-09 22:17:20 -0700

    used useEffect to get data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [chuckJoke, setChuckJoke]=useState([])
+
+  useEffect(() => {
+    axios
+      .get(`http://api.icndb.com/jokes/random/3`)
+      // .then(res => console.log(res.data.value))
+      .then(res => setChuckJoke(res.data.value))
+      .catch(error => {
+        console.log("error", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
+    <div >
+      {chuckJoke.map(haha => 
+        <p key={haha.id}>{haha.joke}</p>)}
     </div>
   );
 }
